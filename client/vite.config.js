42b89e7776b250b9715c92f2d8e04d55b6ca6ee8
yuvarajsadam <yuvarajsadam006@gmail.com>
2025-12-11 16:55:58 +0530

    Change API proxy target to production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://skillswap-ffy2.onrender.com',
         changeOrigin: true,
         secure: false,
       }
